b5183664d29dc00bdb7bf327d4f6aa3793105f91
EmiTis Yousefi <emitis89@yahoo.com>
2017-05-24 18:06:31 +0430

    better classification for main page columns

--- resources/views/front/home/hot-links.blade.php
+++ resources/views/front/home/hot-links.blade.php
@@ -2,106 +2,24 @@
     <div class="row mt15 mb20">
         <div class="col-xs-12 col-md-4">
             <a href="#" class="floating-top-20 floating-left-15 link-red">بیشتر</a>
-            <h3 class="underlined-heading">
-                <span class="text-red border-bottom-red">اخبار داغ</span>
-            </h3>
-            <div class="owl-carousel news-slider" dir="ltr">
-                <div class="item" dir="rtl">
-                    <a href="#" target="_blank">
-                        <img src="https://ehda.center/assets/photos/posts/slideshow/ED21600x600.jpg">
-                        <div class="slide-text">
-                            <h4>«این لحظه خوش را از تو دارم»</h4>
-                            <span>بهرام فرهادی ۳۸ ساله - گیرنده ریه</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="item" dir="rtl">
-                    <a href="#" target="_blank">
-                        <img src="https://ehda.center/assets/photos/posts/slideshow/afshin.jpg">
-                        <div class="slide-text">
-                            <h4>«این لحظه خوش را از تو دارم»</h4>
-                            <span>
-                                بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه
-                            </span>
-                        </div>
-                    </a>
-                </div>
-                <div class="item" dir="rtl">
-                    <a href="#" target="_blank">
-                        <img src="https://ehda.center/assets/photos/posts/slideshow/s02.jpg">
-                        <div class="slide-text">
-                            <h4>«این لحظه خوش را از تو دارم»</h4>
-                            <span>
-                                بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه بهرام فرهادی ۳۸ ساله - گیرنده ریه
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            @include('front.frame.underlined_heading', [
+                'text' => 'اخبار داغ',
+                'color' => 'red',
+            ])
+            @include('front.home.hot_news')
         </div>
         <div class="col-xs-12 col-md-4">
-            <a href="#" class="floating-top-20 floating-left-15 link-green">بیشتر</a>
-            <h3 class="underlined-heading">
-                <span class="text-green border-bottom-green">رویدادها</span>
-            </h3>
-            <div class="media-list">
-                @for($i = 0; $i < 6; $i++)
-                    <div class="media-list-item border-right-green">
-                        <a href="#">
-                            <div class="media-list-item-image">
-                                <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWJhZDg3YjRhNSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1YmFkODdiNGE1Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuNSI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4="
-                                     class="media-object">
-                            </div>
-                            <div class="media-list-item-body">
-                                <h5 class="media-list-item-heading">
-                                    آمار بالای نارسایی کلیه در کشور/ دستگاه‌های دیالیز فرسوده‌اند
-                                </h5>
-                                <p class="text-gray text-left">۲۵ اسفند ۹۴</p>
-                            </div>
-                        </a>
-                    </div>
-                @endfor
-            </div>
+            @include('front.home.posts_list_view', [
+                'title' => 'رویدادها',
+                'color' => 'green'
+            ])
         </div>
         <div class="col-xs-12 col-md-4">
-            <a href="#" class="floating-top-20 floating-left-15 link-blue">بیشتر</a>
-            <h3 class="underlined-heading">
-                <span class="text-blue border-bottom-blue">اخبار اهدا</span>
-            </h3>
-            <div class="media-list">
-                @for($i = 0; $i < 6; $i++)
-                    <div class="media-list-item border-right-blue">
-                        <a href="#">
-                            <div class="media-list-item-image">
-                                <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWJhZDg3YjRhNSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1YmFkODdiNGE1Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuNSI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4="
-                                     class="media-object">
-                            </div>
-                            <div class="media-list-item-body">
-                                <h5 class="media-list-item-heading">
-                                    آمار بالای نارسایی کلیه در کشور/ دستگاه‌های دیالیز فرسوده‌اند
-                                </h5>
-                                <p class="text-gray text-left">۲۵ اسفند ۹۴</p>
-                            </div>
-                        </a>
-                    </div>
-                @endfor
-            </div>
+            @include('front.home.posts_list_view', [
+                'title' => 'اخبار اهدا',
+                'color' => 'blue'
+            ])
         </div>
     </div>
 </div>
 
-<script>
-    $(document).ready(function () {
-        $(".news-slider").owlCarousel({
-            autoPlay: 3000,
-            stopOnHover: false,
-            navigation: true,
-            paginationSpeed: 1000,
-            goToFirstSpeed: 2000,
-            singleItem: true,
-            transitionStyle: "fade",
-            theme: "owl-card-theme",
-            navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
-        });
-    });
-</script>
